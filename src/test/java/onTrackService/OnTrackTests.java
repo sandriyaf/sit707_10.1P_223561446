1317601e9595ff8c7257e6b50c7e6fa7e22c414d
Sandriya <sandriyafernandes10@gmail.com>
2024-05-21 18:25:42 +1000

    Update OnTrackTests.java

--- src/test/java/onTrackService/OnTrackTests.java
+++ src/test/java/onTrackService/OnTrackTests.java
@@ -10,7 +10,7 @@ public class OnTrackTests {
         OnTrackService.Task task = taskService.createTask("Math Homework", "Solve problems 1-10", "student1");
 
         assertNotNull(task, "Task should not be null");
-        assertEquals(8, task.getId(), "Task ID should be 1");
+        assertEquals(5, task.getId(), "Task ID should be 1");
         assertEquals("Math Homework", task.getTitle(), "Task title should match");
         assertEquals("Solve problems 1-10", task.getDescription(), "Task description should match");
         assertEquals("student1", task.getCreator(), "Task creator should match");
